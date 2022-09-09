3a44a6352932ec714732c8e0970a099d91a38e8e
Angela M Julian <angela.julian@gmail.com>
2022-09-08 22:55:53 -0700

    02-05 complete.

--- problems/04-my-filter.js
+++ problems/04-my-filter.js
@@ -19,12 +19,24 @@ let result2 = myFilter(['choose', 'big', 'words', 'only'], function (s) {
 console.log(result2);      // ['choose', 'words', 'only']
 *******************************************************************************/
 
-let myFilter = function() {
-
+let myFilter = function(arr, cb) {
+    const newArr = []
+    for (item in arr) {
+        if (cb(arr[item])) newArr.push(arr[item]);
+    }
+    return newArr;
 };
 
 
+let result1 = myFilter([5, 7, 4, 3, 8], function (n) {
+    return n % 2 === 0;
+});
+console.log(result1);       // [ 4, 8 ]
 
+let result2 = myFilter(['choose', 'big', 'words', 'only'], function (s) {
+    return s.length > 3;
+});
+console.log(result2);      // ['choose', 'words', 'only']
 
 
 
